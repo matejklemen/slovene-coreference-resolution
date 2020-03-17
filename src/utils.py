f86dfda16e501b6c1dcd4963c82b85967197dcd7
Matej Klemen <matej.klemen1337@gmail.com>
2020-03-17 16:46:30 +0100

    Add method for obtaining mention candidates and labels

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,3 @@
-def compare_mentions(m1, m2):
-    # mentions are contiguous spans of text, so if length, first and last el. match, they are equal
-    return len(m1) == len(m2) and m1[0] == m2[0] and m1[-1] == m2[-1]
-
-
 def mention_candidates(sentences, width, end=None):
     """ Extracts mention candidates, consisting of `width` tokens, that appear in interval [0, end) of tokens.
     For example, [['This', 'is', 'a', 'sentence']] with width=2 and end=3 returns mentions [0, 2]
@@ -32,15 +27,34 @@ def mention_candidates(sentences, width, end=None):
     return mentions
 
 
+def mention_dataset(doc, width):
+    """ Prepares mentions dataset with labels (based on ground truth information).
+    Arguments
+    ---------
+    doc: data.Document
+        Document instance
+
+    Returns
+    -------
+    tuple:
+        mention candidates and their labels (is mention / is not mention)
+    """
+    candidates = mention_candidates(doc.sents, width=width)
+    mention_set = set(map(lambda start_end: (start_end[0], start_end[1]),
+                          doc.mentions.values()))
+    is_mention = [int((s, e) in mention_set) for s, e in candidates]
+    return candidates, is_mention
+
+
 if __name__ == "__main__":
-    s = [["The", "way", "I", "see", "it", ",", "every", "life", "is", "a", "pile", "of", "good", "things", "and",
-          "bad", "things", "."],
-         ["The", "good", "things", "do", "n’t", "always", "soften", "the", "bad", "things", ",", "but", "vice",
-          "versa", ",", "the", "bad", "things", "do", "n’t", "always", "spoil", "the", "good", "things", "and",
-          "make", "them" "unimportant", "."]]
+    sample_sent = [["The", "way", "I", "see", "it", ",", "every", "life", "is", "a", "pile", "of", "good", "things",
+                    "and", "bad", "things", "."],
+                   ["The", "good", "things", "do", "n’t", "always", "soften", "the", "bad", "things", ",", "but", "vice",
+                    "versa", ",", "the", "bad", "things", "do", "n’t", "always", "spoil", "the", "good", "things", "and",
+                    "make", "them" "unimportant", "."]]
 
     # Ad-hoc test - check that 5 spans get returned in interval [0, 5)
-    mentions = mention_candidates(s, end=5, width=1)
+    mentions = mention_candidates(sample_sent, end=5, width=1)
     assert mentions[0][0] == 0 and mentions[0][1] == 1
     assert mentions[-1][0] == 4 and mentions[-1][1] == 5
     assert len(mentions) == 5
